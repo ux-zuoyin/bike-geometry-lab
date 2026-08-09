8266fd335d3691cee0a1388ec4505de156709cd1
zy1641043413 <1641043413@qq.com>
2026-08-09 15:45:51 +0800

    Fix asset paths for custom domain

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/bike-geometry-lab/",
   build: {
     outDir: "dist/client",
   },
